aff53314bc6b804b172916441b0cae4aa43dad4e
Anurag Singh <principalquantum30@gmail.com>
2023-08-31 15:07:20 +0530

    JS

---
+++ 3-JavaScript/2-Variable.js
@@ -0,0 +1,10 @@
+let age = 15;//numbers
+let name = "Oval";//string
+let male = true; //boolean
+console.log(age);
+console.log(male);
+console.log(name);
+
+document.getElementById("p1").innerHTML = "Hello " + name;
+document.getElementById("p2").innerHTML = "You are " + age + " Years old";
+document.getElementById("p3").innerHTML = "You are  " + male;
