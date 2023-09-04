521b67169dc824dde8abea450f873bb31b535e60
Anurag Singh <principalquantum30@gmail.com>
2023-09-04 18:42:52 +0530

    JS

---
+++ 3-JavaScript/3-Arithematic-Expression.js
@@ -0,0 +1,7 @@
+let std = 20;
+std = std+1;
+let extra = std%4;
+console.log(std);
+console.log(extra);
+//self assignemnt also subs , multiplication , etc
+//there is operator precendence parenthesis  , exponent , mult& div , add&sub
---
+++ 3-JavaScript/5-Type-Conversion.js
@@ -0,0 +1,7 @@
+let age = window.prompt("how are old are You");//string type because of window prompt
+age = Number(age);
+age+=1;
+console.log(age);
+
+//non empty bool return true else it return false "" is empty
+//nan for things you want to convert that is not the things specified
