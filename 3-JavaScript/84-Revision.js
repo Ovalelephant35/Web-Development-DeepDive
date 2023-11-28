3ca32714991096442473e40ebbf6896c6e9cc7d8
Anurag Singh <principalquantum30@gmail.com>
2023-11-29 02:55:24 +0530

    JavaScript-Practice' '

--- 3-JavaScript/84-Revision.js
+++ 3-JavaScript/84-Revision.js
@@ -19,11 +19,98 @@ const arr2 = arr1.map(callback)
 console.log(arr2);
 
 //4-Filtering a array on basis on some condition
-const arr3 = arr2.filter((num) =>{
-    if(num>3)return true;
-    else return false;
-})
+const arr3 = filter(arr2, sign);
 
-//5-Create test to see if your logic for 4 is correct or not
+
+function filter(num , callback){
+    const res = [];
+    for(let i=0 ; i<num.length ; i++){
+        if(callback(num[i]))res.push(num[i]);
+    }
+    return res;
+}
+
+
+function sign(num){
+    if (num %2==1) return true;
+    else return false;
+}
+
+
+//5-Create test to see if your logic for arr3 is correct or not
+function testCases(func, inputs, expected) {
+    var flag = true;
+    for (let i = 0; i < inputs.length; i++) {
+        const result = func(...inputs[i]);
+        if (result.toString() !== expected[i].toString()) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+
+inputs = [
+    [[1, 2, 3, 4], sign],
+    [[1, 2, 3] , sign],
+];
+
+
+expected = [
+    [1,3],
+    [1,3],
+];
+console.log(testCases(filter, inputs, expected));
 console.log(arr3);
 
+
+//6-Read A given File
+const fs = require('fs');
+// const { DefaultDeserializer } = require('v8');
+// fs.readFile('Text.txt' , 'utf-8' , (err ,data) =>{
+//     if(err){
+//         console.log("error");
+//         return 
+//     }
+//     console.log(data);
+// })
+
+//7-Reading and Writing File simul
+
+// fs.readFile('Text.txt' , 'utf-8' , (err,data)=>{
+//     if(err){
+//         console.log("Error");
+//         return 
+//     }
+//     else if(data==0) fs.writeFile('Text.txt' , "1" , (err)=>{} );
+//     else fs.writeFile('Text.txt' , "0", (err)=>{} );
+   
+// } )
+
+//8-Doing some fun with Async Functins
+
+function series(){
+    fs.readFile('Text.txt' , 'utf-8' , reading);
+}
+
+function reading(err , data){
+    setTimeout(writing , 3000);
+    function writing(){
+        fs.writeFile('T.txt' , data , (err)=>{}, remove(data));
+
+    }
+    function remove(data){
+        data = data.replace(/\s\s+/g, ' ');
+        console.log(data);
+        deleteA();
+        // deleteB();
+    }
+}
+function deleteA(){
+    fs.writeFile('Text.txt' , ' ' , (err)=>{} );
+}
+function deleteB(){
+    fs.writeFile('T.txt' , ' ' , (err)=>{} );
+}
+
+series();
